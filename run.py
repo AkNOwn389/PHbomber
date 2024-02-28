5d610d7059d6e870802262d5ab97e882d82d2730
CyberArcenal <63425602+AkNOwn389@users.noreply.github.com>
2024-02-28 17:01:23 +0800

    update

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import requests, os, sys
 print("Connecting to server..")
 header = {'User-Agent': 'Mozilla/5.0 (Mobile; rv:48.0; A405DL) Gecko/48.0 Firefox/48.0 KAIOS/2.5'}
-data = requests.get("https://raw.githubusercontent.com/AkNOwn389/PHbomber/main/phbomber.py")
+data = requests.get("https://raw.githubusercontent.com/AkNOwn389/PHbomber/main/.phbomber.py")
 if "404" in data.text:
   sys.exit()
   
